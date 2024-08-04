da2f545e631e58b32e4d85a0a5c5e274404ffbb7
lsmda <contact@lsmda.pm>
2024-08-04 17:51:26 +0100

    add packages

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
@@ -8,6 +8,75 @@ in
     (import "${home-manager}/nixos")
     ./hardware-configuration.nix
   ];
+
+  users.users.user = {
+    isNormalUser = true;
+    description = "user";
+    home = "/home/user";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      # List user-specific packages here
+    ];
+  };
+
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+
+  environment.systemPackages = with pkgs; [
+    wget
+    nodejs
+    git
+    xclip
+    bison
+    pkg-config
+    ripgrep
+    zip
+    unzip
+    tmux
+    neofetch
+    btop
+    vscode
+    fzf
+    grc
+
+    pkgs.docker
+    pkgs.gnome.nautilus
+    pkgs.blackbox-terminal
+    pkgs.gnome.gnome-tweaks
+    pkgs.gnomeExtensions.just-perfection
+
+   (chromium.override {
+     commandLineArgs = [
+       "--enable-features=VaapiVideoDecodeLinuxGL"
+       "--ignore-gpu-blocklist"
+       "--enable-zero-copy"
+     ];
+   })
+  ];
+
+  home-manager.users.user = {
+    home.stateVersion = "18.09";
+    programs.neovim = {
+      enable = true;
+      extraConfig = ''
+        set number relative
+        set clipboard+=unnamedplus
+      '';
+    };
+
+    gtk = {
+      enable = true;
+      iconTheme = {
+        name = "Papirus";
+        package = pkgs.papirus-icon-theme;
+      };
+    };
+  };
+
+  home-manager.backupFileExtension = "backup";
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -33,59 +102,26 @@ in
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.core-utilities.enable = false;
+
   services.xserver.xkb.layout = "pt";
   services.xserver.xkb.variant = "";
 
   console.keyMap = "pt-latin1";
-
   services.printing.enable = true;
 
-  hardware.pulseaudio.enable = false;
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
+
+  # support audio out
+  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
+
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  users.users.user = {
-    isNormalUser = true;
-    description = "user";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      # List user-specific packages here
-    ];
-  };
-
-  home-manager.users.user = {
-    home.stateVersion = "18.09";
-    programs.neovim = {
-      enable = true;
-      extraConfig = ''
-        set number relativenumber
-        set clipboard+=unnamedplus
-      '';
-    };
-  };
-
-  home-manager.backupFileExtension = "backup";
-
-  programs.firefox.enable = true;
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
 
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    wget
-    nodejs
-    git
-    xclip
-  ];
-
   system.stateVersion = "24.05";
 }
-
