4cb7501864029bd99b6303fe02a8cdf4d1d36e9b
lsmda <contact@lsmda.pm>
2024-08-04 17:51:26 +0100

    update fish shell init

--- configuration.nix
+++ configuration.nix
@@ -96,8 +96,21 @@ in {
     programs.kitty.shellIntegration.enableFishIntegration = true;
 
     programs.fish.enable = true;
-    programs.fish.interactiveShellInit = ''
+    programs.fish.shellInit = ''
       set fish_greeting # Disable greeting
+
+      # Check if TMUX is unset or empty and if the session is interactive
+      if test -z "$TMUX" && status is-interactive
+          # Check if any Tmux sessions exist
+          set tmux_sessions (tmux list-sessions)
+          if test -z "$tmux_sessions"
+              # If no sessions exist, start a new Tmux session
+              tmux new-session
+          else
+              # If sessions exist, attach to the first one
+              tmux attach-session -t (echo $tmux_sessions[1] | cut -d: -f1)
+          end
+      end
     '';
     programs.fish.functions = {
       deleteGenerationsRange.body = ''
