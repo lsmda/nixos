4fe72e53adfd9f97fc34fc4bda1f8c091174bcac
lsmda <contact@lsmda.pm>
2024-08-04 17:51:26 +0100

    chore: update fish functions

--- configuration.nix
+++ configuration.nix
@@ -214,9 +214,20 @@ in {
               end
             end
           '';
-          removeNixGenerations.body = ''
-            for i in (seq (math $argv[1]) (math $argv[2]))
-              sudo nix-env --delete-generations $i --profile /nix/var/nix/profiles/system
+          removeNixGeneration.body = ''
+            set -l start $argv[1]
+            set -l end $argv[2]
+
+            if test (count $argv) -eq 1
+              # Only one argument provided, delete that single generation
+              sudo nix-env --delete-generations $start --profile /nix/var/nix/profiles/system
+            else if test (count $argv) -eq 2
+              # Two arguments provided, delete the range of generations
+              for i in (seq (math $start) (math $end))
+                sudo nix-env --delete-generations $i --profile /nix/var/nix/profiles/system
+              end
+            else
+              echo "Usage: removeNixGeneration <start> [end]"
             end
           '';
           listNixGenerations.body = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
