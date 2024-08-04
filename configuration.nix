5bf2a0230cc61ce928afcf163dd0bda341f7f3d1
lsmda <contact@lsmda.pm>
2024-08-04 17:51:26 +0100

    update main config

--- configuration.nix
+++ configuration.nix
@@ -193,25 +193,6 @@ in {
     pulse.enable = true;
   };
 
-  # GNOME 46: Triple Buffering
-  nixpkgs.overlays = [
-    (final: prev: {
-      gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
-        mutter = gnomePrev.mutter.overrideAttrs (old: {
-          src = pkgs.fetchFromGitLab {
-            domain = "gitlab.gnome.org";
-            owner = "vanvugt";
-            repo = "mutter";
-            rev = "triple-buffering-v4-46";
-            hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
-          };
-        });
-      });
-    })
-  ];
-
-  nixpkgs.config.allowAliases = false;
   nixpkgs.config.allowUnfree = true;
-
   system.stateVersion = "24.05";
 }
