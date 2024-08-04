85eaa7384194319eb4a28504b2c3f14c7429af40
lsmda <contact@lsmda.pm>
2024-08-04 17:51:26 +0100

    updates

--- configuration.nix
+++ configuration.nix
@@ -9,14 +9,13 @@ in
     ./hardware-configuration.nix
   ];
 
+  users.groups.docker = {};
+
   users.users.user = {
     isNormalUser = true;
     description = "user";
     home = "/home/user";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    packages = with pkgs; [
-      # List user-specific packages here
-    ];
+    extraGroups = [ "networkmanager" "wheel" "docker"];
   };
 
   programs.fish.enable = true;
@@ -25,33 +24,36 @@ in
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
 
-  environment.gnome.excludePackages = [ pkgs.gnome-tour pkgs.xterm ];
+  environment.gnome.excludePackages = [ pkgs.gnome-tour ];
 
   environment.systemPackages = with pkgs; [
     wget
     nodejs
-    git
     xclip
     bison
-    pkg-config
     ripgrep
     zip
     unzip
     tmux
-    neofetch
+    pkg-config
     btop
-    vscode
     fzf
     grc
+    neofetch
+    vscode
 
-    pkgs.gnome.gnome-terminal
-    pkgs.docker
-    pkgs.spotify
-    pkgs.qbittorrent
-    pkgs.protonvpn-gui
-    pkgs.gnome.nautilus
-    pkgs.gnome.gnome-tweaks
-    pkgs.gnomeExtensions.just-perfection
+    docker
+    spotify
+    qbittorrent
+    home-manager
+    protonvpn-gui
+    gnome.nautilus
+    tela-icon-theme
+    gnome.gnome-tweaks
+    gnome.gnome-terminal
+    git-credential-oauth
+    gnomeExtensions.just-perfection
+    gnomeExtensions.appindicator
 
    (chromium.override {
      commandLineArgs = [
@@ -65,33 +67,52 @@ in
   home-manager.users.user = {
     home.username = "user";
     home.homeDirectory = "/home/user";
-    
+
+    programs.fish.enable = true;
     programs.neovim.enable = true;
     programs.neovim.extraLuaConfig = ''
     ${builtins.readFile ./nvim/lua/lsmda/core/options.lua} 
     ${builtins.readFile ./nvim/lua/lsmda/core/keymaps.lua} 
     '';
 
-    gtk = {
-      enable = true;
-      iconTheme = {
-        name = "Papirus";
-        package = pkgs.papirus-icon-theme;
-      };
-    };
+    programs.gnome-shell.enable = true;
+    programs.home-manager.enable = true;
+
+    programs.git.enable = true;
+    programs.git.userName = "lsmda";
+    programs.git.userEmail = "lsmda@apollo.pm";
+    programs.git.extraConfig.credential.helper = "oauth";
+
+    gtk.enable = true;
+    gtk.iconTheme.name = "Tela-grey-dark";
+
+    dconf.enable = true;
+    dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 
     home.stateVersion = "24.05";
   };
 
   home-manager.backupFileExtension = "backup";
 
+  fonts.packages = with pkgs; [
+    (pkgs.nerdfonts.override {
+      fonts = [ "JetBrainsMono" ];
+    })
+  ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
   
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.gnome.core-utilities.enable = false;
+  services.gnome.gnome-keyring.enable = true;
+
+  services.udev.packages = with pkgs; [
+    gnome.gnome-settings-daemon 
+  ];
 
   networking.hostName = "machine";
   networking.networkmanager.enable = true;
@@ -117,18 +138,33 @@ in
   console.keyMap = "pt-latin1";
   services.printing.enable = true;
 
-  # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.support32Bit = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
 
-  # support audio out
-  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
-
   security.rtkit.enable = true;
 
   nixpkgs.config.allowUnfree = true;
+
+  # GNOME 46: triple-buffering-v4-46
+  nixpkgs.overlays = [
+    (final: prev: {
+      gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+        mutter = gnomePrev.mutter.overrideAttrs (old: {
+          src = pkgs.fetchFromGitLab  {
+            domain = "gitlab.gnome.org";
+            owner = "vanvugt";
+            repo = "mutter";
+            rev = "triple-buffering-v4-46";
+            hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
+          };
+        });
+      });
+    })
+  ];
+
+  nixpkgs.config.allowAliases = false;
 
   system.stateVersion = "24.05";
 }
