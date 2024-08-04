1ac3801b0abf191e8f301d77449f900d1056466c
lsmda <contact@lsmda.pm>
2024-08-04 17:51:26 +0100

    update home-manager module

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -5,7 +5,7 @@ in {
     (import "${home-manager}/nixos")
   ];
 
-  home-manager.users.user = {
+  home-manager.users.user = {lib, ...}: {
     home.username = "user";
     home.homeDirectory = "/home/user";
 
@@ -23,12 +23,63 @@ in {
       };
     };
 
-    gtk.enable = true;
-    gtk.iconTheme.name = "Tela-grey-dark";
-    gtk.iconTheme.package = pkgs.tela-icon-theme;
+    gtk = {
+      enable = true;
+      iconTheme = {
+        name = "Tela-grey-dark";
+        package = pkgs.tela-icon-theme;
+      };
+    };
 
-    dconf.enable = true;
-    dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    dconf = let
+      background = "file:///home/user/dotfiles/wallpapers/background_00.jpg";
+    in {
+      enable = true;
+      settings = {
+        # Displays
+        "org/gnome/settings-daemon/plugins/color" = {
+          night-light-enabled = true;
+          night-light-schedule-automatic = false;
+          night-light-schedule-from = 0.0;
+          night-light-schedule-to = 0.0;
+          night-light-temperature = lib.hm.gvariant.mkUint32 4200;
+        };
+        # Power
+        "org/gnome/control-center" = {
+          last-panel = "power";
+        };
+        "org/gnome/settings-daemon/plugins/power" = {
+          sleep-inactive-ac-type = "nothing";
+          power-button-action = "nothing";
+        };
+        "org/gnome/desktop/session" = {
+          idle-delay = lib.hm.gvariant.mkUint32 0;
+        };
+        # Multitasking
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          enable-hot-corners = false;
+        };
+        "org/gnome/mutter" = {
+          edge-tiling = true;
+          workspaces-only-on-primary = false;
+        };
+        "org/gnome/shell/app-switcher" = {
+          current-workspace-only = true;
+        };
+        # Appearence
+        "org/gnome/desktop/background" = {
+          picture-uri = background;
+          picture-uri-dark = background;
+        };
+        "org/gnome/desktop/screensaver" = {
+          picture-uri = background;
+        };
+        "org/gnome/nautilus/icon-view" = {
+          default-zoom-level = "large";
+        };
+      };
+    };
 
     home.stateVersion = "24.05";
   };
