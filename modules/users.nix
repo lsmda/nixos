7db88d54816a89f5a18a107221525413ecd86b51
lsmda <contact@lsmda.pm>
2024-09-04 18:45:46 +0100

    update users module

--- modules/users.nix
+++ modules/users.nix
@@ -6,7 +6,8 @@
 
     users."user" = {
       isNormalUser = true;
-      description = "user";
+      uid = 1000;
+      group = "users";
       home = "/home/user";
       hashedPassword = "$y$j9T$2gy4AbwUx4sj0UgSt5vza.$2HEAct5Ip.X1x9f0uYDDCTNbabDbAl8aZ78yDdSS/h5";
       extraGroups = ["networkmanager" "wheel" "docker"];
