fd951a63e4ed0da29a4196b948dd25e2908872f1
lsmda <contact@lsmda.pm>
2024-09-06 01:41:44 +0100

    update pipewire module

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -9,7 +9,28 @@
       alsa.support32Bit = true;
       pulse.enable = true;
 
-      wireplumber.extraConfig = {
+      wireplumber.extraConfig.disable-session-timeout = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              {"node.name" = "~alsa_input.*";}
+              {"node.name" = "~alsa_output.*";}
+            ];
+            actions.update-props = {
+              "node.pause-on-idle" = false;
+              "session.suspend-timeout-seconds" = 0;
+            };
+          }
+        ];
+      };
+
+      wireplumber.extraConfig.bluetooth-enhancements = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+        };
       };
     };
   };
