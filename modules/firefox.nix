12201b99fb3e16ad65a2116c76971b24b59c355e
lsmda <contact@lsmda.pm>
2024-10-06 22:08:12 +0100

    update firefox module

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -29,10 +29,12 @@
           "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
             "installation_mode" = "force_installed";
             "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+            "default_area" = "navbar";
           };
           "uBlock0@raymondhill.net" = {
             "installation_mode" = "force_installed";
             "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            "default_area" = "navbar";
           };
         };
 
