f4a456c8299b842459e4c2fc2e2a0b1eb4fff23c
lsmda <contact@lsmda.pm>
2024-10-06 20:25:00 +0100

    update git module

--- modules/git.nix
+++ modules/git.nix
@@ -1,10 +1,48 @@
 {
   enable = true;
+
   userName = "lsmda";
   userEmail = "contact@lsmda.pm";
+
   extraConfig = {
     credential.credentialStore = "secretservice";
     credential.helper = [ "manager" ];
     core.commentChar = ";";
+  };
+
+  aliases = {
+    a = "add";
+    aa = "add --all";
+
+    c = "checkout";
+    cb = "checkout -b";
+
+    s = "status";
+    l = "log --oneline";
+
+    cm = "commit -m";
+    ca = "commit --amend";
+
+    f = "fetch";
+    fp = "fetch --purge"; # delete merged local branches
+
+    bd = "branch -D"; # delete local branch
+    bdr = "branch push --delete"; # delete remote branch
+
+    pl = "pull";
+    ps = "push";
+    pf = "push --force-with-lease";
+
+    rs = "reset HEAD~"; # undo last local commit
+    rv = "revert";
+    rt = "restore"; # discard changes at path
+
+    mr = "merge";
+    rb = "rebase";
+
+    st = "stash";
+    sd = "stash drop";
+    sp = "stash pop";
+    sa = "stash push --all";
   };
 }
