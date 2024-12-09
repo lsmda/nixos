c30c9623faea65f7ef71f58f3389091343d40a70
lsmda <contact@lsmda.pm>
2024-12-09 23:59:09 +0000

    add teams pwa

--- home/chromium.nix
+++ home/chromium.nix
@@ -1,14 +1,7 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
-{
-  programs.chromium.enable = true;
-  programs.chromium.package = pkgs.ungoogled-chromium;
-
-  programs.chromium.dictionaries = with pkgs.hunspellDictsChromium; [
-    en_US
-  ];
-
-  programs.chromium.commandLineArgs = [
+let
+  chromium_flags = [
     # performance
     "--enable-gpu-rasterization"
     "--enable-oop-rasterization"
@@ -30,10 +23,38 @@
     "--disable-speech-synthesis-api"
   ];
 
+  parsed_args = builtins.concatStringsSep " " chromium_flags;
+
+  teams_chromium = pkgs.makeDesktopItem {
+    name = "Teams";
+    desktopName = "Teams";
+    genericName = "Microsoft Teams";
+    exec = ''${config.programs.chromium.package}/bin/chromium ${parsed_args} --app="https://teams.microsoft.com"'';
+    icon = "teams";
+    categories = [
+      "Network"
+      "InstantMessaging"
+    ];
+    mimeTypes = [ "x-scheme-handler/teams" ];
+  };
+in
+
+{
+  programs.chromium.enable = true;
+  programs.chromium.package = pkgs.chromium;
+
+  programs.chromium.dictionaries = with pkgs.hunspellDictsChromium; [
+    en_US
+  ];
+
+  programs.chromium.commandLineArgs = chromium_flags;
+
   programs.chromium.extensions = [
     { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # proton pass
     { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
     { id = "fmkadmapgofadopljbjfkapdkoienihi"; } # react developer tools
     { id = "lmhkpmbekcpmknklioeibfkpmmfibljd"; } # redux devtools
   ];
+
+  home.packages = [ teams_chromium ];
 }
