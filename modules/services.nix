13b216dd218a93b12a06da0908929144ca8f0f68
lsmda <contact@lsmda.pm>
2025-03-15 10:00:48 +0000

    add iteration of ssh service

--- modules/services.nix
+++ modules/services.nix
@@ -8,8 +8,25 @@ in
   systemd.services.ssh-authorized-keys = {
     script = ''
       SPELLBOOK_KEY=$(${pkgs.coreutils}/bin/cat ${secrets."spellbook/ed_25519_pub".path})
-      echo $SPELLBOOK_KEY >> ~/.ssh/authorized_keys
-      echo \n >> ~/.ssh/authorized_keys
+
+      # Define the authorized_keys file path
+      AUTH_KEYS_FILE=~/.ssh/authorized_keys
+
+      # Check if directory exists, create if it doesn't
+      [ -d ~/.ssh ] || mkdir -p ~/.ssh
+
+      # If file doesn't exist, create it
+      # If it does exist, clear its contents
+      if [ ! -f "$AUTH_KEYS_FILE" ]; then
+        touch "$AUTH_KEYS_FILE"
+        chmod 600 "$AUTH_KEYS_FILE"
+      else
+        : > "$AUTH_KEYS_FILE"  # This clears the file contents
+      fi
+
+      # Append the key and add a newline
+      echo "$SPELLBOOK_KEY" >> "$AUTH_KEYS_FILE"
+      echo "" >> "$AUTH_KEYS_FILE"  # Adds a blank line
     '';
 
     serviceConfig = {
