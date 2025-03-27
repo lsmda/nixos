80d5bfb9c108f61f84f25557bcbed45242d3e13e
lsmda <contact@lsmda.pm>
2025-03-27 22:00:30 +0000

    update ssh config

--- home/nushell.nix
+++ home/nushell.nix
@@ -79,40 +79,13 @@ in
       }
     }
 
-    def ssh-key [keys: list<string>] {
-      keychain --eval --quiet ...$keys
-        | lines
-        | where not ($it | is-empty)
-        | parse "{k}={v}; export {k2};"
-        | select k v
-        | transpose --header-row
-        | into record
-        | load-env
-    }
-
-    do --env {
-        let ssh_agent_file = (
-            $nu.temp-path | path join $"ssh-agent-($env.USER? | default $env.USERNAME).nuon"
-        )
-
-        if ($ssh_agent_file | path exists) {
-            let ssh_agent_env = open ($ssh_agent_file)
-            if ($"/proc/($ssh_agent_env.SSH_AGENT_PID)" | path exists) {
-                load-env $ssh_agent_env
-                return
-            } else {
-                rm $ssh_agent_file
-            }
-        }
-
-        let ssh_agent_env = ^ssh-agent -c
-            | lines
-            | first 2
-            | parse "setenv {name} {value};"
-            | transpose --header-row
-            | into record
-        load-env $ssh_agent_env
-        $ssh_agent_env | save --force $ssh_agent_file
-    }
+    keychain --eval --quiet ~/.ssh/server, ~/.ssh/laptop, ~/.ssh/desktop
+      | lines
+      | where not ($it | is-empty)
+      | parse "{k}={v}; export {k2};"
+      | select k v
+      | transpose --header-row
+      | into record
+      | load-env
   '';
 }
