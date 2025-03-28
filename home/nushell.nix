6f0a6464905a6b04c0c854cc2a281e3b85928f5e
lsmda <contact@lsmda.pm>
2025-03-28 17:17:28 +0000

    update config

--- home/nushell.nix
+++ home/nushell.nix
@@ -9,8 +9,41 @@ in
   programs.nushell.enable = true;
 
   programs.nushell.configFile.text = ''
+    $env.PROMPT_COMMAND = {||
+        let dir = match (do -i { $env.PWD | path relative-to $nu.home-path }) {
+            null => $env.PWD
+            "" => '~'
+            $relative_pwd => ([~ $relative_pwd] | path join)
+        }
+
+        let path_color = (if (is-admin) { ansi red_bold } else { ansi green_bold })
+        let separator_color = (if (is-admin) { ansi light_red_bold } else { ansi light_green_bold })
+        let path_segment = $"($path_color)($dir)(ansi reset)"
+
+        $path_segment | str replace --all (char path_sep) $"($separator_color)(char path_sep)($path_color)"
+    }
+
+
+    $env.PROMPT_COMMAND_RIGHT = {||
+        let time_segment = ([
+            (ansi reset)
+            (ansi magenta)
+            (date now | format date '%d/%m/%Y %H:%m')
+        ] | str join | str replace --regex --all "([/:])" $"(ansi green)''${1}(ansi magenta)" |
+            str replace --regex --all "([AP]M)" $"(ansi magenta_underline)''${1}")
+
+        let last_exit_code = if ($env.LAST_EXIT_CODE != 0) {([
+            (ansi rb)
+            ($env.LAST_EXIT_CODE)
+        ] | str join)
+        } else { "" }
+
+        ([$last_exit_code, (char space), $time_segment] | str join)
+    }
+
+    $env.EDITOR = "hx"
     $env.OPENAI_API_KEY = (open ${secrets."user/deepseek".path})
-    $env.config.show_banner = false
+    $env.config.show_banner = false # disable welcome message
     $env.config.completions.external.enable = true
     $env.config.completions.external.max_results = 200
 
