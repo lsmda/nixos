53acec7cc17c20b3fb504045613b2cb4d16499d5
lsmda <contact@lsmda.pm>
2025-04-03 01:24:57 +0100

    fix sops module

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -29,23 +29,23 @@ in
   sops.age.keyFile = "/home/${user}/.config/sops/age/keys.txt";
 
   # user's hashed password
-  sops.secrets."user/password" = from_yaml ../secrets/sys.yaml;
-  sops.secrets."user/deepseek" = from_yaml ../secrets/sys.yaml;
+  sops.secrets."user/password" = from_yaml ../../secrets/sys.yaml;
+  sops.secrets."user/deepseek" = from_yaml ../../secrets/sys.yaml;
 
   # ssh public keys
-  sops.secrets."spellbook/ed_25519_pub" = from_yaml ../secrets/ssh.yaml;
-  sops.secrets."thornmail/ed_25519_pub" = from_yaml ../secrets/ssh.yaml;
-  sops.secrets."wardstone/ed_25519_pub" = from_yaml ../secrets/ssh.yaml;
+  sops.secrets."spellbook/ed_25519_pub" = from_yaml ../../secrets/ssh.yaml;
+  sops.secrets."thornmail/ed_25519_pub" = from_yaml ../../secrets/ssh.yaml;
+  sops.secrets."wardstone/ed_25519_pub" = from_yaml ../../secrets/ssh.yaml;
 
   # git credentials
-  sops.secrets."git/main" = from_binary ../secrets/git/main.conf;
-  sops.secrets."git/work" = from_binary ../secrets/git/work.conf;
+  sops.secrets."git/main" = from_binary ../../secrets/git/main.conf;
+  sops.secrets."git/work" = from_binary ../../secrets/git/work.conf;
 
   # wireguard configuration files
-  sops.secrets."es_62" = from_binary ../secrets/wireguard/es_62.conf;
-  sops.secrets."es_65" = from_binary ../secrets/wireguard/es_65.conf;
-  sops.secrets."ie_25" = from_binary ../secrets/wireguard/ie_25.conf;
-  sops.secrets."ie_36" = from_binary ../secrets/wireguard/ie_36.conf;
-  sops.secrets."uk_14" = from_binary ../secrets/wireguard/uk_14.conf;
-  sops.secrets."uk_24" = from_binary ../secrets/wireguard/uk_24.conf;
+  sops.secrets."es_62" = from_binary ../../secrets/wireguard/es_62.conf;
+  sops.secrets."es_65" = from_binary ../../secrets/wireguard/es_65.conf;
+  sops.secrets."ie_25" = from_binary ../../secrets/wireguard/ie_25.conf;
+  sops.secrets."ie_36" = from_binary ../../secrets/wireguard/ie_36.conf;
+  sops.secrets."uk_14" = from_binary ../../secrets/wireguard/uk_14.conf;
+  sops.secrets."uk_24" = from_binary ../../secrets/wireguard/uk_24.conf;
 }
