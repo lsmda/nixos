85b9d6e6f9252adef950f39e49ed9f08b7315f53
lsmda <contact@lsmda.pm>
2025-04-04 22:02:25 +0100

    update comment

--- modules/system/pipewire.nix
+++ modules/system/pipewire.nix
@@ -10,7 +10,7 @@
   services.pipewire.pulse.enable = true;
 
   # on laptop, the `node.pause-on-idle` action is called when no sound is being played.
-  # this action produces an annoying 'click' sound, which becomes even more annoying when
+  # this action produces an annoying 'pop' sound, which becomes even more annoying when
   # you hear it hundreds of times during your session. disabling it to keep my sanity.
   services.pipewire.wireplumber.extraConfig.disable-session-timeout."monitor.alsa.rules" = [
     {
