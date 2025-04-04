3b26d1a5d85b3f9992fb9f095f611a8beb0681c4
lsmda <contact@lsmda.pm>
2025-04-04 22:02:19 +0100

    refactor

--- utils/default.nix
+++ utils/default.nix
@@ -1,10 +1,10 @@
-let
-  with_name = name: { inherit name; };
-  with_value = name: with_name name // { value = { }; };
+{ ... }:
 
-  # convert a string to an attribute set
-  # "a" -> { name = "a"; value = {}; }
-  to_attribute = name: with_value name;
+let
+  to_attribute = str: {
+    name = str;
+    value = { };
+  };
 in
 
 {
