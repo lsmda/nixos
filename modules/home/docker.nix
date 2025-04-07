73fa2a38ca746e9d1d6fc688ba3ac749b5edcd65
lsmda <contact@lsmda.pm>
2025-04-07 12:57:47 +0100

    add imagesFormat config

--- modules/home/docker.nix
+++ modules/home/docker.nix
@@ -6,7 +6,8 @@
     docker
   ];
 
-  home.file.".docker/config.json".text = ''
-    { "psFormat": "table {{.RunningFor}}\t{{.State}}\t{{printf \"%.50s\" .Image}}\t{{.ID}}\t{{.Names}}" }
-  '';
+  home.file.".docker/config.json".text = builtins.toJSON {
+    psFormat = "table {{.RunningFor}}\t{{.State}}\t{{printf \"%.50s\" .Image}}\t{{.ID}}\t{{.Names}}";
+    imagesFormat = "table {{printf \"%.50s\" .Repository}}\t{{.Tag}}\t{{.ID}}\t{{.CreatedSince}}\t{{.Size}}";
+  };
 }
