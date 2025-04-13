6504289fe142498b78dba2d90665fbb227a4c00a
lsmda <contact@lsmda.pm>
2025-04-13 13:18:20 +0100

    update settings

--- modules/system/soft-serve.nix
+++ modules/system/soft-serve.nix
@@ -1,8 +1,12 @@
 {
+  environmnet.variables = {
+    SOFT_SERVE_DATA_PATH = "/srv/soft-serve";
+  };
+
   services.soft-serve = {
     enable = true;
     settings = {
-      name = "lsmda's repos";
+      name = "Runestore";
       log_format = "text";
       ssh = {
         listen_addr = ":23231";
