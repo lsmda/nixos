c1ca5455ec2785b5c737102a81d335054e82071f
lsmda <contact@lsmda.pm>
2025-04-13 20:25:55 +0100

    remove systemd module

--- machines/spellbook/default.nix
+++ machines/spellbook/default.nix
@@ -35,7 +35,6 @@ in
     ../../modules/system/options.nix
     ../../modules/system/pipewire.nix
     ../../modules/system/sops.nix
-    ../../modules/system/systemd.nix
     ../../modules/system/xserver.nix
   ];
 
