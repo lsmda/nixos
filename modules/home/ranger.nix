1e0df4ff8d9d67b42d3241603745ee6c41658952
lsmda <contact@lsmda.pm>
2025-04-13 12:05:30 +0100

    update settings

--- modules/home/ranger.nix
+++ modules/home/ranger.nix
@@ -1,65 +1,42 @@
 { pkgs, ... }:
 
-let
-  inherit (pkgs) fetchFromGitHub;
-in
-
 {
   programs.ranger = {
     enable = true;
 
     settings = {
-      viewmode = "miller";
-      column_ratios = "2,2,4";
+      column_ratios = "2,2,6";
       draw_borders = "both";
+      scroll_offset = 10;
       unicode_ellipsis = true;
+      viewmode = "miller";
+
+      collapse_preview = false;
+      preview_directories = true;
+      preview_files = true;
       preview_images = true;
       preview_images_method = "kitty";
-      preview_files = true;
-      preview_directories = true;
-      collapse_preview = true;
+
+      vcs_aware = true;
+      vcs_backend_git = "local";
     };
 
     extraPackages = with pkgs; [
-      atool
+      atool # Archive CLI helper
       ffmpeg
-      ffmpegthumbnailer
-      highlight
-      jq
-      mediainfo
-      mpv
+      ffmpegthumbnailer # Video thumbnailer
+      highlight # Source code highlight
+      jq # JSON processor
+      mediainfo # technical info on media files
+      mpv # media player
       p7zip
       rclone
-      w3m
     ];
 
-    plugins = [
+    rifle = [
       {
-        name = "ranger-archives";
-        src = fetchFromGitHub {
-          owner = "maximtrp";
-          repo = "ranger-archives";
-          rev = "b4e136b24fdca7670e0c6105fb496e5df356ef25";
-          sha256 = "sha256-QJu5G2AYtwcaE355yhiG4wxGFMQvmBWvaPQGLsi5x9Q=";
-        };
-      }
-      {
-        name = "devicons2";
-        src = fetchFromGitHub {
-          owner = "cdump";
-          repo = "ranger-devicons2";
-          rev = "f7877aa0dd8caa1d498d935f6f49e57a4fc591e2";
-          sha256 = "sha256-OMMQW/mn8J8mki41TD/7/CWaDFgp/zT7B2hfTH/m0Ug=";
-        };
-      }
-      {
-        name = "ranger-fzf-filter";
-        src = fetchFromGitHub {
-          owner = "MuXiu1997";
-          repo = "ranger-fzf-filter";
-          rev = "bf16de2e4ace415b685ff7c58306d0c5146f9f43";
-          sha256 = "sha256-4J0OLNeXPKvS7WvpgGWJPOeecXFG0QJ5/GbM3qogFTk=";
-        };
+        condition = "ext (md|markdown), has glow";
+        command = "glow --pager \"$@\"";
       }
     ];
   };
