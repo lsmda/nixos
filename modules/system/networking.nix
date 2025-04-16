c60ef5dcff69aa28e7d73c5ddc125d9e8e041596
lsmda <contact@lsmda.pm>
2025-04-16 17:05:06 +0100

    add local hosts

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -16,4 +16,11 @@ in
   networking.networkmanager.enable = true;
 
   networking.useDHCP = lib.mkDefault true;
+
+  networking.extraHosts = ''
+    192.168.0.1   router
+    192.168.0.5   wardstone
+    192.168.0.10  thornmail
+    192.168.0.11  spellbook
+  '';
 }
