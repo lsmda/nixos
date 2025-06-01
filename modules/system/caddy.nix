e26664f220f288861110a80e3a05f94f6e0f524d
lsmda <contact@lsmda.pm>
2025-06-01 13:51:50 +0100

    use ip from config

--- modules/system/caddy.nix
+++ modules/system/caddy.nix
@@ -8,7 +8,7 @@
 
   services.caddy = {
     enable = true;
-    virtualHosts."http://192.168.0.5:80".extraConfig = ''
+    virtualHosts."http://${config.lan.storage}:80".extraConfig = ''
       respond "{\"status\": \"operational\", \"host\": \"${config.machine.hostname}\"}"
     '';
   };
