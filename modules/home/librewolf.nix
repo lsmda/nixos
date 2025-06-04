12211121be692f57cdddf481ac5b91e4d94664b3
lsmda <contact@lsmda.pm>
2025-06-04 20:58:38 +0100

    update settings

--- modules/home/librewolf.nix
+++ modules/home/librewolf.nix
@@ -2,9 +2,7 @@
   programs.librewolf = {
     enable = true;
     settings = {
-      "browser.download.autohideButton" = true;
-      "browser.theme.dark-private-windows" = false;
-      "identity.fxaccounts.enabled" = true;
+      "identity.fxaccounts.enabled" = false;
       "image.jxl.enabled" = true; # Enable JPEG XL support
       "media.ffmpeg.vaapi.enabled" = true; # Enable VA-API hard accelaration
       "middlemouse.paste" = false;
@@ -12,24 +10,21 @@
       "webgl.disabled" = false;
       "browser.download.panel.shown" = true;
       "network.http.referer.XOriginPolicy" = 2;
-      "media.autoplay.blocking_policy" = 2;
       "security.OCSP.require" = false; # disable ocsp hard-fail
       "widget.use-xdg-desktop-portal.mime-handler" = 1; # set system file dialog
       "dom.w3c.touch_events.enabled" = true; # touch support aktivieren
-      "browser.download.improvements_to_download_panel" = false; # disable "fast" Download
       "browser.backspace_action" = 0; # Browser Backspace enable
       "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # enable theming
       "general.autoScroll" = true; # enable autoscroll
       "media.navigator.enabled" = false;
       "xpinstall.signatures.required" = false;
+      "intl.accept_languages" = "en-US";
       "widget.use-xdg-desktop-portal.file-pcker" = 1;
-      "intl.accept_languages" = "de,en-US";
-      "network.dns.disableIPv6" = true;
       "privacy.firstparty.isolate" = false;
       "geo.enabled" = false;
       "dom.security.https_only_mode_ever_enabled" = true;
       "media.eme.enabled" = true;
-      "privacy.resistFingerprinting" = false;
+      "privacy.resistFingerprinting" = true;
       "network.captive-portal-service.enabled" = true;
       "security.enterprise_roots.enabled" = true;
     };
