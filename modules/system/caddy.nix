cced09659ba8a33d8de9fa68097a65dd4f38a984
lsmda <contact@lsmda.pm>
2025-06-14 13:32:51 +0100

    setup wildcard subdomains

--- modules/system/caddy.nix
+++ modules/system/caddy.nix
@@ -15,7 +15,10 @@ in
     tunnels = {
       "a12fef7a-4df6-40c8-beb7-5f2bca981024" = {
         credentialsFile = "${secrets."cloudflared/lsmda.pm".path}";
-        ingress."lsmda.pm" = "http://localhost:5000";
+        ingress = {
+          "lsmda.pm" = "http://localhost:5000";
+          "*.lsmda.pm" = "http://localhost:5001";
+        };
         originRequest.httpHostHeader = "localhost";
         default = "http_status:404";
       };
@@ -26,14 +29,38 @@ in
     enable = true;
     virtualHosts = {
       "http://localhost:80".extraConfig = ''
-        respond "{ \"status\": \"ok\" }"
+        bind 127.0.0.1 [::1]
+        respond "{ \"status\": \"OK\" }"
       '';
       "http://localhost:5000".extraConfig = ''
+        bind 127.0.0.1 [::1]
+
         root * /var/www/lsmda.pm
-        file_server
         encode gzip
+        file_server
+
         log {
-          output file /var/log/caddy/lsmda-access.log
+          output file /var/log/caddy/lsmda.pm
+        }
+      '';
+      "http://localhost:5001".extraConfig = ''
+        bind 127.0.0.1 [::1]
+
+        @resume {
+          header X-Forwarded-Host resume.lsmda.pm
+        }
+
+        handle @resume {
+          reverse_proxy http://localhost:5000
+        }
+
+        # refuse unknown subdomains
+        handle {
+          respond 404
+        }
+
+        log {
+          output file /var/log/caddy/test.lsmda.pm
         }
       '';
     };
