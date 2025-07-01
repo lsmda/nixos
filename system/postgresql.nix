4253b50d3cb191f035465ac801e103d7416742cb
lsmda <contact@lsmda.pm>
2025-07-01 19:29:00 +0100

    remove stale code

--- system/postgresql.nix
+++ system/postgresql.nix
@@ -1,28 +1,8 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 
 let
   inherit (import ../utils { inherit lib; }) createUsersGroups;
   serviceUser = "postgresql";
-
-  sql-file = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/pthom/northwind_psql/c1035fc5b5dfa45f164a7bc4a1632656f0025642/northwind.sql";
-    sha256 = "1dk9x2hfikw2xpnl2v3j5r8k2cc32srldg2z7c2z20m17aamykpp";
-  };
-
-  northwind-sql = pkgs.stdenv.mkDerivation {
-    pname = "northwind-sql";
-    version = "0.0.1";
-    builder = pkgs.writeText "builder.sh" ''
-      . $stdenv/setup
-      mkdir -pv $out/share/
-      cp ${sql-file} $out/share/northwind.sql
-    '';
-    meta = with lib; {
-      description = "The Microsoft Northwind database.";
-      homepage = "https://github.com/pthom/northwind_psql";
-      platforms = platforms.all;
-    };
-  };
 in
 
 {
@@ -46,13 +26,5 @@ in
         ensureClauses.createdb = true;
       }
     ];
-    initialScript = pkgs.writeText "initScript" ''
-      CREATE USER ice;
-      CREATE DATABASE ice OWNER ice;
-      GRANT ALL PRIVILEGES ON DATABASE ice TO "ice";
-      \connect ice
-      SET ROLE 'ice';
-      \i /home/user/chinook.sql
-    '';
   };
 }
