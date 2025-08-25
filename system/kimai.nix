fb549a6084dd5f3306b7fef4080924a5039853ba
lsmda <contact@lsmda.pm>
2025-08-25 18:54:28 +0100

    chore (kimai): add local.yaml configuration file

--- system/kimai.nix
+++ system/kimai.nix
@@ -8,6 +8,7 @@ in
 {
   sops.secrets."kimai" = fromBinary ../secrets/kimai/kimai;
   sops.secrets."kimai-db" = fromBinary ../secrets/kimai/kimai-db;
+  sops.secrets."kimai-local" = fromBinary ../secrets/kimai/kimai-local;
 
   virtualisation.oci-containers.containers."kimai" = {
     image = "kimai/kimai2:apache";
@@ -20,10 +21,12 @@ in
       "8001:8001"
     ];
     volumes = [
-      "/mnt/kimai/cache:/opt/kimai/var/cache"
-      "/mnt/kimai/data:/opt/kimai/var/data"
-      "/mnt/kimai/packages:/opt/kimai/config/packages"
-      "/mnt/kimai/plugins:/opt/kimai/var/plugins"
+      # kimai configuration file
+      "${secrets."kimai-local".path}:/opt/kimai/config/packages/local.yaml:ro"
+
+      "/var/lib/kimai/cache:/opt/kimai/var/cache"
+      "/var/lib/kimai/data:/opt/kimai/var/data"
+      "/var/lib/kimai/plugins:/opt/kimai/var/plugins"
     ];
   };
 
@@ -38,15 +41,17 @@ in
       secrets."kimai-db".path
     ];
     volumes = [
-      "/mnt/kimai-db:/var/lib/mysql"
+      "/var/lib/kimai-db:/var/lib/mysql"
     ];
   };
 
   systemd.tmpfiles.rules = [
-    "d /mnt/kimai/cache 0755 root root"
-    "d /mnt/kimai/data 0755 root root"
-    "d /mnt/kimai/packages 0755 root root"
-    "d /mnt/kimai/plugins 0755 root root"
-    "d /mnt/kimai-db 0755 root root"
+    # web UI
+    "d /var/lib/kimai/cache 0770 root root - -"
+    "d /var/lib/kimai/data 0770 root root - -"
+    "d /var/lib/kimai/plugins 0770 root root - -"
+
+    # database
+    "d /var/lib/kimai-db 0770 root root - -"
   ];
 }
