97abcde1a50279bd1e0a13bce0680827cd13af35
lsmda <contact@lsmda.pm>
2025-10-06 08:47:36 +0100

    chore (machines/spellbook): fix display scaling

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -3,7 +3,7 @@
     programs.ghostty = {
       enable = true;
       settings = {
-        font-size = 14.5;
+        font-size = 14;
         theme = "iceberg-dark";
 
         font-family = "Berkeley Mono"; # "JetBrains Mono"
