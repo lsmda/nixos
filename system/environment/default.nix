c9b962a1f7409a529c48d7723b2703f4805d6401
lsmda <contact@lsmda.pm>
2025-10-11 13:28:35 +0100

    chore (home/niri): move env variables to niri

--- system/environment/default.nix
+++ system/environment/default.nix
@@ -1,23 +1,5 @@
 {
   config = {
-    environment.variables = {
-      MOZ_ENABLE_WAYLAND = 1;
-      ELECTRON_OZONE_PLATFORM_HINT = "auto";
-
-      # Qt theming
-      QT_QPA_PLATFORM = "wayland";
-      QT_QPA_PLATFORMTHEME = "qt6ct";
-      QT_QPA_PLATFORMTHEME_QT6 = "qt6ct";
-
-      XDG_SESSION_TYPE = "wayland";
-      XDG_CURRENT_DESKTOP = "niri";
-
-      # Electron & Chromium
-      NIXOS_OZONE_WL = "1";
-
-      _JAVA_AWT_WM_NONREPARENTING = "1";
-    };
-
     programs.nix-ld.enable = true;
 
     # Enables simultaneous use of processor threads.
