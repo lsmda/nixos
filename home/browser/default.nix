ef3078725fe25967964c3f5954b3ee1dfc3518e6
lsmda <contact@lsmda.pm>
2025-10-21 17:47:03 +0100

    chore (home/browser): add mimeTypes handlers

--- home/browser/default.nix
+++ home/browser/default.nix
@@ -408,5 +408,36 @@
         };
       };
     };
+
+    xdg.mimeApps =
+      let
+        mimeTypes = [
+          "default-web-browser"
+          "application/json"
+          "application/pdf"
+          "application/x-extension-htm"
+          "application/x-extension-html"
+          "application/x-extension-shtml"
+          "application/x-extension-xhtml"
+          "application/x-extension-xht"
+          "application/xhtml+xml"
+          "text/html"
+          "text/xml"
+          "x-scheme-handler/about"
+          "x-scheme-handler/ftp"
+          "x-scheme-handler/http"
+          "x-scheme-handler/unknown"
+          "x-scheme-handler/https"
+        ];
+      in
+      {
+        enable = true;
+        defaultApplications = builtins.listToAttrs (
+          map (mimeType: {
+            name = mimeType;
+            value = [ "firefox.desktop" ];
+          }) mimeTypes
+        );
+      };
   };
 }
